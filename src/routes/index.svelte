2980a1a3c4c1265f606051ff337d13e6e423ede8
Arimations <arieshaa05@gmail.com>
2021-12-19 04:06:43 +0000

    done

---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script>
+
+    let name = "" ;
+    let gender = "" ;
+    
+    async function guessGender() {
+      fetch('https://api.genderize.io/?name=' + name)
+        .then((response) => response.json())
+        .then((data) => {
+          gender = data.gender; // Path for description
+        });
+    }
+    
+    </script>
+    
+    
+    
+    <h1>Guess Gender</h1>
+    
+    <form on:submit|preventDefault={guessGender}>
+        <label for="name">Name:</label>
+        <input id="name" bind:value={name} type="text" />
+        <button type="submit">Check</button>
+    </form>
+
+    
+<p>Gender: {gender}</p>
